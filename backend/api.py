2d759a30dda3b9a4d581e252aa67afa87128dcee
Cory Balaton <cory@balaton.dev>
2022-01-15 13:29:29 +0100

    Changed things to be able to deploy on server

--- backend/api.py
+++ backend/api.py
@@ -139,5 +139,5 @@ def get_image(file: str):
     return send_from_directory(path, file)
     
 
-
-app.run(host="0.0.0.0")
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
--- frontend/app.py
+++ frontend/app.py
@@ -62,5 +62,6 @@ def add_ingredient():
         
     return redirect(url_for("admin"))
     
-    
-app.run(host="0.0.0.0", port=5001)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
